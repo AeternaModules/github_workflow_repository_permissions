41297048de5ecca2da34e26bab0770274fc2a44b
Daniel Allen <dan@aeternagroup.net>
2026-07-16 15:02:41 +0100

    Update to schema version v6.13.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "workflow_repository_permissionses_id" {
   description = "Map of id values across all workflow_repository_permissionses, keyed the same as var.workflow_repository_permissionses"
-  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.id }
+  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "workflow_repository_permissionses_can_approve_pull_request_reviews" {
   description = "Map of can_approve_pull_request_reviews values across all workflow_repository_permissionses, keyed the same as var.workflow_repository_permissionses"
-  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.can_approve_pull_request_reviews }
+  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.can_approve_pull_request_reviews if v.can_approve_pull_request_reviews != null }
 }
 output "workflow_repository_permissionses_default_workflow_permissions" {
   description = "Map of default_workflow_permissions values across all workflow_repository_permissionses, keyed the same as var.workflow_repository_permissionses"
-  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.default_workflow_permissions }
+  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.default_workflow_permissions if v.default_workflow_permissions != null && length(v.default_workflow_permissions) > 0 }
 }
 output "workflow_repository_permissionses_repository" {
   description = "Map of repository values across all workflow_repository_permissionses, keyed the same as var.workflow_repository_permissionses"
-  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.repository }
+  value       = { for k, v in github_workflow_repository_permissions.workflow_repository_permissionses : k => v.repository if v.repository != null && length(v.repository) > 0 }
 }
 
